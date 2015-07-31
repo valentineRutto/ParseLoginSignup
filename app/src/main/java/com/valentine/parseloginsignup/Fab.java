27e56f6173a7c14fb95b0a90d6a91115a9d67b24
valentinerutto <rutto28@gmail.com>
2015-07-31 09:18:18 +0300

    set show fab

--- app/src/main/java/com/valentine/parseloginsignup/Fab.java
+++ app/src/main/java/com/valentine/parseloginsignup/Fab.java
@@ -1,18 +1,34 @@
 package com.valentine.parseloginsignup;
 
+import android.content.Context;
+import android.util.AttributeSet;
+
 import com.gordonwong.materialsheetfab.AnimatedFab;
 
 public class Fab extends FloatingActionButton implements AnimatedFab {
+    private static final int FAB_ANIM_DURATION = 200;
+
+    public Fab(Context context) {
+        super(context);
+    }
+
+    public Fab(Context context, AttributeSet attrs) {
+        sup
+        er(context, attrs);
+    }
+
+    public Fab(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
 
     @Override
     public void show() {
-        // Animate the FAB into view or simply set its visibility
-    }
+        show(0, 0);    }
 
     @Override
     public void show(float translationX, float translationY) {
-        
+
     }
 
 
--- app/src/main/java/com/valentine/parseloginsignup/FloatingActionButton.java
+++ app/src/main/java/com/valentine/parseloginsignup/FloatingActionButton.java
@@ -1,7 +1,12 @@
 package com.valentine.parseloginsignup;
 
+import android.content.Context;
+import android.util.AttributeSet;
+
 /**
  * Created by valentine on 7/31/15.
  */
 public class FloatingActionButton {
+    public FloatingActionButton(Context context, AttributeSet attrs) {
+    }
 }
