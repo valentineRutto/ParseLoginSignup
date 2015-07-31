1067d6ccb45f5b4ec15bcae8d2855197b085cc70
valentinerutto <rutto28@gmail.com>
2015-07-31 09:01:12 +0300

    created the animation class(fab)

---
+++ app/src/main/java/com/valentine/parseloginsignup/Fab.java
@@ -0,0 +1,22 @@
+package com.valentine.parseloginsignup;
+
+import com.gordonwong.materialsheetfab.AnimatedFab;
+
+public class Fab extends FloatingActionButton implements AnimatedFab {
+
+
+    @Override
+    public void show() {
+        // Animate the FAB into view or simply set its visibility
+    }
+
+    @Override
+    public void show(float translationX, float translationY) {
+        
+    }
+
+
+    @Override
+    public void hide() {
+    }
+}
