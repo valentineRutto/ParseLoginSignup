b64e8dbc85cccc761421fe7722efb1884f648a51
valentinerutto <rutto28@gmail.com>
2015-07-31 09:25:29 +0300

    defined hide fab

--- app/src/main/java/com/valentine/parseloginsignup/Fab.java
+++ app/src/main/java/com/valentine/parseloginsignup/Fab.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 import android.view.animation.ScaleAnimation;
 
 import com.gordonwong.materialsheetfab.AnimatedFab;
@@ -16,8 +18,7 @@ public class Fab extends FloatingActionButton implements AnimatedFab {
     }
 
     public Fab(Context context, AttributeSet attrs) {
-        sup
-        er(context, attrs);
+        super(context, attrs);
     }
 
     public Fab(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -33,15 +34,12 @@ public class Fab extends FloatingActionButton implements AnimatedFab {
     public void show(float translationX, float translationY) {
         setTranslation(translationX, translationY);
 
-        // Only use scale animation if FAB is hidden
         if (getVisibility() != View.VISIBLE) {
-            // Pivots indicate where the animation begins from
             float pivotX = getPivotX() + translationX;
             float pivotY = getPivotY() + translationY;
 
             ScaleAnimation anim;
-            // If pivots are 0, that means the FAB hasn't been drawn yet so just use the
-            // center of the FAB
+
             if (pivotX == 0 || pivotY == 0) {
                 anim = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f,
                         Animation.RELATIVE_TO_SELF, 0.5f);
@@ -49,7 +47,6 @@ public class Fab extends FloatingActionButton implements AnimatedFab {
                 anim = new ScaleAnimation(0, 1, 0, 1, pivotX, pivotY);
             }
 
-            // Animate FAB expanding
             anim.setDuration(FAB_ANIM_DURATION);
             anim.setInterpolator(getInterpolator());
             startAnimation(anim);
@@ -61,5 +58,28 @@ public class Fab extends FloatingActionButton implements AnimatedFab {
 
     @Override
     public void hide() {
+        // Only use scale animation if FAB is visible
+        if (getVisibility() == View.VISIBLE) {
+            // Pivots indicate where the animation begins from
+            float pivotX = getPivotX() + getTranslationX();
+            float pivotY = getPivotY() + getTranslationY();
+
+            // Animate FAB shrinking
+            ScaleAnimation anim = new ScaleAnimation(1, 0, 1, 0, pivotX, pivotY);
+            anim.setDuration(FAB_ANIM_DURATION);
+            anim.setInterpolator(getInterpolator());
+            startAnimation(anim);
+        }
+        setVisibility(View.INVISIBLE);
     }
+
+    private void setTranslation(float translationX, float translationY) {
+        animate().setInterpolator(getInterpolator()).setDuration(FAB_ANIM_DURATION)
+                .translationX(translationX).translationY(translationY);
+    }
+
+    private Interpolator getInterpolator() {
+        return AnimationUtils.loadInterpolator(getContext(), R.interpolator.msf_interpolator);
+    }
+
 }
